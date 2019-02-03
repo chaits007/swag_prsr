2b163836d4a0c77552b333eee13d4e817ba15e5e
Chaitanya Vuppuluri <vuppuluri@gmail.com>
2019-02-03 11:29:37 +0530

    Resolved References

--- src/main/java/com/chaitanya/swag_prsr/App.java
+++ src/main/java/com/chaitanya/swag_prsr/App.java
@@ -13,6 +13,8 @@ import io.swagger.models.RefModel;
 import io.swagger.models.Response;
 import io.swagger.models.Swagger;
 import io.swagger.models.parameters.Parameter;
+import io.swagger.models.parameters.RefParameter;
+import io.swagger.models.properties.ArrayProperty;
 import io.swagger.models.properties.Property;
 import io.swagger.models.properties.RefProperty;
 import io.swagger.models.utils.PropertyModelConverter;
@@ -21,70 +23,72 @@ import io.swagger.parser.SwaggerParser;
 
 public class App {
 
-	private static Map<String, Parameter> resp;
-
 	public static void main(String[] args) {
 		String location = "./resources/nasu_spec.json";
 		Swagger swagger = new SwaggerParser().read(location);
+		Map<String, Model> models = swagger.getDefinitions();
 		
-		System.out.println(swagger);
 		//GetPaths and Path Parameters
 		Map<String, Path> paths = swagger.getPaths();
 		for (Entry<String, Path> p : paths.entrySet()) {
+			System.out.println("===============================================================================================================");
+			System.out.println("===============================================================================================================");
+			System.out.println("PATH:" + p.getKey());
 			Path path = p.getValue();
 			Map<HttpMethod, Operation> operations = path.getOperationMap();
 			for (Entry<HttpMethod, Operation> o : operations.entrySet()) {
-				System.out.println("===");
-				System.out.println("PATH:" + p.getKey());
 				System.out.println("Http method:" + o.getKey());
 				System.out.println("Summary:" + o.getValue().getSummary());
-				System.out.println("Parameters number: " + o.getValue().getParameters().size());
-				for (Parameter parameter : o.getValue().getParameters()) {
-					System.out.println("Parm Name: " + parameter.getName()); 
-					System.out.println("Input Type: " + parameter.getIn());
+				
+				System.out.println("\n****************************************** Request Parameters *********************************************");
+				for (Parameter parm : o.getValue().getParameters()) {
+					processRequestParms(swagger, parm);
 				}
-				System.out.println("Responses:");
+				
+				System.out.println("\n****************************************** Response Parameters *********************************************");
 				for (Map.Entry<String, Response> r : o.getValue().getResponses().entrySet()) {
 					System.out.println(" - " + r.getKey() + ": " + r.getValue().getDescription());
-					Model resp_mdl = r.getValue().getResponseSchema();
-					String ref = resp_mdl.getReference();
-					System.out.println("$ref : " + ref);
-//					RefProperty pmc = (RefProperty) new PropertyModelConverter().modelToProperty(resp_mdl);
+					RefModel resp_mdl = (RefModel) r.getValue().getResponseSchema();
 					
-//					//Get Properties for each key
-//					Map<String, Property> props = resp_mdl.getProperties();
-//					for (Entry<String, Property> resp_p : props.entrySet()) {
-//						System.out.println("****Response Property****");
-//						System.out.println("Name: " + resp_p.getKey());
-//						System.out.println("Desc: " + resp_p.getValue().getDescription());
-//						System.out.println("Type: " + resp_p.getValue().getType());
-//						System.out.println("Reqd: " + resp_p.getValue().getRequired());
-//						System.out.println("Reqd: " + resp_p.getValue());	
-//					}
-//				}
+					Model definitions = swagger.getDefinitions().get(resp_mdl.getSimpleRef());
+					//Get Properties for each key
+					Map<String, Property> props = definitions.getProperties();
+					for (Entry<String, Property> p1 : props.entrySet()) {
+						if (p1.getValue().getClass() == ArrayProperty.class) {
+							ArrayProperty temp = (ArrayProperty) p1.getValue();
+							if (temp.getItems().getClass() == RefProperty.class) {
+								RefProperty refP1 = (RefProperty) temp.getItems();
+								System.out.println(p1.getKey()	+ " - "	+ p1.getValue().getType() + " - Has Reference - " + refP1.getOriginalRef());
+								Model propDef = swagger.getDefinitions().get(refP1.getSimpleRef());
+								Map<String, Property> propDefProp = propDef.getProperties();
+								for (Entry<String, Property> p2 : propDefProp.entrySet()) {
+									System.out.println(p2.getKey() + " - " + p2.getValue().getType());	
+								}
+							} else {
+								throw new NullPointerException("Not Ref Property - " + p1.getKey()	+ " - "	+ p1.getValue().getType());
+							}
+							System.out.println("--------------------------------------------------------");
+						} else {
+							System.out.println(p1.getKey()	+ " - "	+ p1.getValue().getType());	
+						}	
+					}
 				}
-			}	
-		}
-		
-		//Get Definitions and Properties
-		Map<String, Model> models = swagger.getDefinitions();
-		System.out.println("\nDefinitions:");
-		for (Entry<String, Model> m : models.entrySet()) {
-			Model definitions = swagger.getDefinitions().get(m.getKey());
-			System.out.println("Key : " + m.getKey());
-			
-			//Get Properties for each key
-			Map<String, Property> props = definitions.getProperties();
-			for (Entry<String, Property> p : props.entrySet()) {
-				System.out.println("****Property****");
-				System.out.println("Name: " + p.getKey());
-				System.out.println("Desc: " + p.getValue().getDescription());
-				System.out.println("Type: " + p.getValue().getType());
-				System.out.println("Reqd: " + p.getValue().getRequired());
-				System.out.println("Reqd: " + p.getValue());	
 			}
-			
-			System.out.println("============================");
-		}		
+		}
+	}
+	
+	public static void processRequestParms(Swagger swagger, Parameter parm) {
+		
+		if (parm.getClass() == RefParameter.class) {
+			RefParameter refParm = (RefParameter) parm;
+			System.out.println("Has Reference - " + refParm.getIn() + " - " + refParm.get$ref()); 
+			Model definitions = swagger.getDefinitions().get(refParm.getSimpleRef());
+			Map<String, Property> props = definitions.getProperties();
+			for (Entry<String, Property> p1 : props.entrySet()) {
+				System.out.println(p1.getKey() + " - " + p1.getValue().getType());	
+			}
+		} else {
+			System.out.println(parm.getIn() + " - " + parm.getName());
+		}
 	}
 }
